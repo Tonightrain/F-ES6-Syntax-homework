6e6f105dfe2367870d9e1bad26fd920e448f0b6d
MSI-PV <805560812@qq.com>
2020-09-12 01:36:06 +0800

    完成练习

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,13 @@
-const parseData
+const parseData = (jsonData) => {
+    const {data, column} = jsonData;
+    const result = [];
+    for (let detail of data){
+        let resultData = {};
+        column.forEach((value,index) => {
+            resultData[value.name] = detail[index];
+        })
+        result.push(resultData);
+    }
+    return result;
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,13 @@
-const inject
+const inject =(items, sections) => {
+    let currentIndex = 0;
+    for(let section of sections){
+        if (section.index <= currentIndex){
+            items.splice(section.index,0,section.content)
+        }else {
+            items.splice(section.index+1,0,section.content)
+            currentIndex = section.index;
+        }
+    }
+    return items;
+}
 export { inject };
